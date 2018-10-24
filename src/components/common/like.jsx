232d147c78f12fca7dcdee0eb2b9cae6cbffe856
Alex <alernerm@gmail.com>
2018-10-24 15:33:35 -0400

    added heart icon

---
+++ src/components/common/like.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+//Input: likes: bool
+//Output: onClick event
+
+class Like extends Component {
+  render() {
+    return <i className="fa fa-heart-o" aria-hidden="true" />;
+  }
+}
+
+export default Like;
