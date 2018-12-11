e4a8b550a332cfa67f30fc1bcce5f9899be2c95a
Alex <alernerm@gmail.com>
2018-12-11 08:45:49 -0500

    back button

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const MovieForm = () => {
-  return <h1> MovieForm</h1>;
+const MovieForm = ({ match, history }) => {
+  //get id from queryString
+  return (
+    <div>
+      <h1> Movie Form {match.params.id}</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => history.push('/movies')}>
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default MovieForm;
